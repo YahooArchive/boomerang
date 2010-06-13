a1a3ca37b37c24dd0f9a520700f62088dea7e45e
Philip Tellis <philip@bluesmoon.info>
2010-06-13 16:24:18 -0700

    add howto for measuring in-page times

--- doc/howtos/howtos.js
+++ doc/howtos/howtos.js
@@ -3,6 +3,8 @@
 BOOMR.subscribe('before_beacon', function(o) {
 	var html = "";
 	if(o.t_done) { html += "This page took " + o.t_done + "ms to load<br>"; }
+	if(o.t_head) { html += "The head section loaded in " + o.t_head + "ms<br>"; }
+	if(o.t_body) { html += "The body section loaded in " + o.t_body + "ms<br>"; }
 	if(o.bw) { html += "Your bandwidth to this server is " + parseInt(o.bw/1024) + "kbps (&#x00b1;" + parseInt(o.bw_err*100/o.bw) + "%)<br>"; }
 	if(o.lat) { html += "Your latency to this server is " + parseInt(o.lat) + "&#x00b1;" + o.lat_err + "ms<br>"; }
 
