0fe90f3bee514b684cef4501e5a68d08cb1e60ef
Philip Tellis <philip@bluesmoon.info>
2010-06-14 01:28:06 -0700

    t_head and t_body are not param vars

--- doc/howtos/howtos.js
+++ doc/howtos/howtos.js
@@ -3,8 +3,7 @@
 BOOMR.subscribe('before_beacon', function(o) {
 	var html = "";
 	if(o.t_done) { html += "This page took " + o.t_done + "ms to load<br>"; }
-	if(o.t_head) { html += "The head section loaded in " + o.t_head + "ms<br>"; }
-	if(o.t_body) { html += "The body section loaded in " + o.t_body + "ms<br>"; }
+	if(o.t_other) { html += "Other timers measured: " + o.t_other + "<br>"; }
 	if(o.bw) { html += "Your bandwidth to this server is " + parseInt(o.bw/1024) + "kbps (&#x00b1;" + parseInt(o.bw_err*100/o.bw) + "%)<br>"; }
 	if(o.lat) { html += "Your latency to this server is " + parseInt(o.lat) + "&#x00b1;" + o.lat_err + "ms<br>"; }
 
