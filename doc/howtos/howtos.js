1ed17ffc5b2cda45f4c8953d87bb3c8671a9a9c4
Philip Tellis <philip@bluesmoon.info>
2010-06-16 12:30:25 -0700

    add extra paramters to beacon

--- doc/howtos/howtos.js
+++ doc/howtos/howtos.js
@@ -1,7 +1,7 @@
 // Since we don't set a beacon_url, we'll just subscribe to the before_beacon function
 // and print the results into the browser itself.
 BOOMR.subscribe('before_beacon', function(o) {
-	var html = "", t_name, t_other;
+	var html = "", t_name, t_other, others = [];
 	if(o.t_done) { html += "This page took " + o.t_done + " ms to load<br>"; }
 	if(o.t_other) {
 		t_other = o.t_other.replace(/\|/, ' = ').split(',');
@@ -13,6 +13,26 @@ BOOMR.subscribe('before_beacon', function(o) {
 	if(o.bw) { html += "Your bandwidth to this server is " + parseInt(o.bw/1024) + "kbps (&#x00b1;" + parseInt(o.bw_err*100/o.bw) + "%)<br>"; }
 	if(o.lat) { html += "Your latency to this server is " + parseInt(o.lat) + "&#x00b1;" + o.lat_err + "ms<br>"; }
 
-	document.getElementById('results').innerHTML = html;
+	var r = document.getElementById('results');
+	r.innerHTML = html;
+
+	for(var k in o) {
+		if(!k.match(/^(t_done|t_other|bw|lat|bw_err|lat_err)$/)) {
+			others.push(k + "=" + o[k]);
+		}
+	}
+
+	if(others.length) {
+		r.innerHTML += "Other parameters:<br>";
+
+		for(var i=0; i<others.length; i++) {
+			var t = document.createTextNode(others[i]);
+			r.innerHTML += "&nbsp;&nbsp;&nbsp;";
+			r.appendChild(t);
+			r.innerHTML += "<br>";
+			
+		}
+	}
+
 });
 
