d3c51db036598f187388430060094176ad0370c0
Philip Tellis <philip@bluesmoon.info>
2010-06-16 01:40:28 -0700

    wrap at 80

--- boomerang.js
+++ boomerang.js
@@ -597,12 +597,16 @@ var d=w.document;
 BOOMR = BOOMR || {};
 BOOMR.plugins = BOOMR.plugins || {};
 
-// We choose image sizes so that we can narrow down on a bandwidth range as soon as possible
-// the sizes chosen correspond to bandwidth values of 14-64kbps, 64-256kbps, 256-1024kbps, 1-2Mbps, 2-8Mbps, 8-30Mbps & 30Mbps+
+// We choose image sizes so that we can narrow down on a bandwidth range as
+// soon as possible the sizes chosen correspond to bandwidth values of
+// 14-64kbps, 64-256kbps, 256-1024kbps, 1-2Mbps, 2-8Mbps, 8-30Mbps & 30Mbps+
 // Anything below 14kbps will probably timeout before the test completes
-// Anything over 60Mbps will probably be unreliable since latency will make up the largest part of download time
-// If you want to extend this further to cover 100Mbps & 1Gbps networks, use image sizes of 19,200,000 & 153,600,000 bytes respectively
-// See https://spreadsheets.google.com/ccc?key=0AplxPyCzmQi6dDRBN2JEd190N1hhV1N5cHQtUVdBMUE&hl=en_GB for a spreadsheet with the details
+// Anything over 60Mbps will probably be unreliable since latency will make up
+// the largest part of download time. If you want to extend this further to
+// cover 100Mbps & 1Gbps networks, use image sizes of 19,200,000 & 153,600,000
+// bytes respectively
+// See https://spreadsheets.google.com/ccc?key=0AplxPyCzmQi6dDRBN2JEd190N1hhV1N5cHQtUVdBMUE&hl=en_GB
+// for a spreadsheet with the details
 var images=[
 	{ name: "image-0.png", size: 11483, timeout: 1400 }, 
 	{ name: "image-1.png", size: 40658, timeout: 1200 }, 
@@ -616,7 +620,9 @@ var images=[
 images.end = images.length;
 images.start = 0;
 
-// abuse arrays to do the latency test simply because it avoids a bunch of branches in the rest of the code
+// abuse arrays to do the latency test simply because it avoids a bunch of
+// branches in the rest of the code.
+// I'm sorry Douglas
 images.l = { name: "image-l.gif", size: 35, timeout: 1000 };
 
 // private object
@@ -676,14 +682,16 @@ var impl = {
 			std_dev, std_err,
 			lat_filtered;
 	
-		// We first do IQR filtering and use the resulting data set for all calculations
+		// We first do IQR filtering and use the resulting data set
+		// for all calculations
 		lat_filtered = this.iqr(this.latencies.sort(this.ncmp));
 		n = lat_filtered.length;
 	
 		this.debug(lat_filtered);
 	
 		// First we get the arithmetic mean, standard deviation and standard error
-		// We ignore the first since it paid the price of DNS lookup, TCP connect and slow start
+		// We ignore the first since it paid the price of DNS lookup, TCP connect
+		// and slow start
 		for(i=1; i<n; i++) {
 			sum += lat_filtered[i];
 			sumsq += lat_filtered[i] * lat_filtered[i];
@@ -703,7 +711,9 @@ var impl = {
 	
 		n = lat_filtered.length-1;
 	
-		median = Math.round((lat_filtered[Math.floor(n/2)] + lat_filtered[Math.ceil(n/2)])/2);
+		median = Math.round(
+				(lat_filtered[Math.floor(n/2)] + lat_filtered[Math.ceil(n/2)]) / 2
+			);
 	
 		return { mean: amean, median: median, stddev: std_dev, stderr: std_err };
 	},
@@ -724,11 +734,13 @@ var impl = {
 	
 			r=this.results[i].r;
 	
-			// the next loop we iterate through backwards and only consider the largest 3 images that succeeded
-			// that way we don't consider small images that downloaded fast without really saturating the network
+			// the next loop we iterate through backwards and only consider the largest
+			// 3 images that succeeded that way we don't consider small images that
+			// downloaded fast without really saturating the network
 			nimgs=0;
 			for(j=r.length-1; j>=0 && nimgs<3; j--) {
-				if(typeof r[j] === 'undefined') {	// if we hit an undefined image time, it means we skipped everything before this
+				// if we hit an undefined image time, we skipped everything before this
+				if(typeof r[j] === 'undefined') {
 					break;
 				}
 				if(r[j].t === null) {
@@ -738,7 +750,8 @@ var impl = {
 				n++;
 				nimgs++;
 	
-				bw = images[j].size*1000/r[j].t;	// multiply by 1000 since t is in milliseconds and not seconds
+				// multiply by 1000 since t is in milliseconds and not seconds
+				bw = images[j].size*1000/r[j].t;
 				bandwidths.push(bw);
 	
 				bw_c = images[j].size*1000/(r[j].t - this.latency.mean);
@@ -751,7 +764,8 @@ var impl = {
 		this.debug('bandwidths: ' + bandwidths);
 		this.debug('corrected: ' + bandwidths_corrected);
 	
-		// First do IQR filtering since we use the median here and should use the stddev after filtering.
+		// First do IQR filtering since we use the median here
+		// and should use the stddev after filtering.
 		if(bandwidths.length > 3) {
 			bandwidths = this.iqr(bandwidths.sort(this.ncmp));
 			bandwidths_corrected = this.iqr(bandwidths_corrected.sort(this.ncmp));
@@ -763,7 +777,8 @@ var impl = {
 		this.debug('after iqr: ' + bandwidths);
 		this.debug('corrected: ' + bandwidths_corrected);
 	
-		// Now get the mean & median.  Also get corrected values that eliminate latency
+		// Now get the mean & median.
+		// Also get corrected values that eliminate latency
 		n = Math.max(bandwidths.length, bandwidths_corrected.length);
 		for(i=0; i<n; i++) {
 			if(i<bandwidths.length) {
@@ -783,7 +798,9 @@ var impl = {
 		std_dev = Math.round(std_dev);
 	
 		n = bandwidths.length-1;
-		median = Math.round((bandwidths[Math.floor(n/2)] + bandwidths[Math.ceil(n/2)])/2);
+		median = Math.round(
+				(bandwidths[Math.floor(n/2)] + bandwidths[Math.ceil(n/2)]) / 2
+			);
 	
 		n = bandwidths_corrected.length;
 		amean_corrected = Math.round(sum_corrected/n);
@@ -792,7 +809,12 @@ var impl = {
 		std_dev_corrected = std_dev_corrected.toFixed(2);
 	
 		n = bandwidths_corrected.length-1;
-		median_corrected = Math.round((bandwidths_corrected[Math.floor(n/2)] + bandwidths_corrected[Math.ceil(n/2)])/2);
+		median_corrected = Math.round(
+					(
+						bandwidths_corrected[Math.floor(n/2)]
+						+ bandwidths_corrected[Math.ceil(n/2)]
+					) / 2
+				);
 	
 		this.debug('amean: ' + amean + ', median: ' + median);
 		this.debug('corrected amean: ' + amean_corrected + ', median: ' + median_corrected);
@@ -817,18 +839,44 @@ var impl = {
 	
 	load_img: function(i, run, callback)
 	{
-		var	url = this.base_url + images[i].name + '?t=' + (new Date().getTime()) + Math.random(),
-			timer=0, tstart=0,
-			img = new Image(),
-			that=this;
+		var url = this.base_url + images[i].name
+			+ '?t=' + (new Date().getTime()) + Math.random(),
+		    timer=0, tstart=0,
+		    img = new Image(),
+		    that=this;
 	
-		img.onload=function() { img.onload=img.onerror=null; img=null; clearTimeout(timer); if(callback) { callback.call(that, i, tstart, run, true); } that=callback=null; };
-		img.onerror=function() { img.onload=img.onerror=null; img=null; clearTimeout(timer); if(callback) { callback.call(that, i, tstart, run, false); } that=callback=null; };
+		img.onload=function() {
+			img.onload=img.onerror=null;
+			img=null;
+			clearTimeout(timer);
+			if(callback) {
+				callback.call(that, i, tstart, run, true);
+			}
+			that=callback=null;
+		};
+		img.onerror=function() {
+			img.onload=img.onerror=null;
+			img=null;
+			clearTimeout(timer);
+			if(callback) {
+				callback.call(that, i, tstart, run, false);
+			}
+			that=callback=null;
+		};
 	
-		// the timeout does not abort download of the current image, it just sets an end of loop flag so we don't attempt download of the next image
-		// we still need to wait until onload or onerror fire to be sure that the image download isn't using up bandwidth.
-		// This also saves us if the timeout happens on the first image.  If it didn't, we'd have nothing to measure.
-		timer=setTimeout(function() { if(callback) { callback.call(that, i, tstart, run, null); } }, images[i].timeout + Math.min(400, this.latency ? this.latency.mean : 400));
+		// the timeout does not abort download of the current image, it just sets an
+		// end of loop flag so we don't attempt download of the next image we still
+		// need to wait until onload or onerror fire to be sure that the image
+		// download isn't using up bandwidth.  This also saves us if the timeout
+		// happens on the first image.  If it didn't, we'd have nothing to measure.
+		timer=setTimeout(function() {
+					if(callback) {
+						callback.call(that, i, tstart, run, null);
+					}
+				},
+				images[i].timeout
+					+ Math.min(400, this.latency ? this.latency.mean : 400)
+			);
 	
 		tstart = new Date().getTime();
 		img.src=url;
@@ -844,7 +892,8 @@ var impl = {
 			var lat = new Date().getTime() - tstart;
 			this.latencies.push(lat);
 		}
-		// if we've got all the latency images at this point, we can calculate latency
+		// if we've got all the latency images at this point,
+		// so we can calculate latency
 		if(this.latency_runs === 0) {
 			this.latency = this.calc_latency();
 		}
@@ -862,22 +911,33 @@ var impl = {
 			return;
 		}
 	
-		if(success === null) {			// if timeout, then we set the next image to the end of loop marker
+		// if timeout, then we set the next image to the end of loop marker
+		if(success === null) {
 			this.results[this.nruns-run].r[i+1] = {t:null, state: null, run: run};
 			return;
 		}
 	
-		var result = { start: tstart, end: new Date().getTime(), t: null, state: success, run: run };
+		var result = {
+				start: tstart,
+				end: new Date().getTime(),
+				t: null,
+				state: success,
+				run: run
+			};
 		if(success) {
 			result.t = result.end-result.start;
 		}
 		this.results[this.nruns-run].r[i] = result;
 	
-		// we terminate if an image timed out because that means the connection is too slow to go to the next image
-		if(i >= images.end-1 || typeof this.results[this.nruns-run].r[i+1] !== 'undefined') {
+		// we terminate if an image timed out because that means the connection is
+		// too slow to go to the next image
+		if(i >= images.end-1
+			|| typeof this.results[this.nruns-run].r[i+1] !== 'undefined'
+		) {
 			this.debug(this.results[this.nruns-run]);
-			// First run is a pilot test to decide what the largest image that we can download is
-			// All following runs only try to download this image
+			// First run is a pilot test to decide what the largest image
+			// that we can download is. All following runs only try to
+			// download this image
 			if(run === this.nruns) {
 				images.start = i;
 			}
@@ -904,10 +964,22 @@ var impl = {
 		this.complete = true;
 		BOOMR.sendBeacon();
 	
-		// If we have an IP address we can make the BA cookie persistent for a while because we'll
-		// recalculate it if necessary (when the user's IP changes).
+		// If we have an IP address we can make the BA cookie persistent for a while
+		// because we'll recalculate it if necessary (when the user's IP changes).
 		if(!isNaN(o.bw)) {
-			BOOMR.utils.setCookie(this.cookie, { ba: Math.round(o.bw), be: o.bw_err, l: o.lat, le: o.lat_err, ip: this.user_ip, t: Math.round(new Date().getTime()/1000) }, (this.user_ip ? this.cookie_exp : 0), "/", null);
+			BOOMR.utils.setCookie(this.cookie,
+						{
+							ba: Math.round(o.bw),
+							be: o.bw_err,
+							l: o.lat,
+							le: o.lat_err,
+							ip: this.user_ip,
+							t: Math.round(new Date().getTime()/1000)
+						},
+						(this.user_ip ? this.cookie_exp : 0),
+						"/",
+						null
+				);
 		}
 	
 		this.running = false;
@@ -965,7 +1037,8 @@ BOOMR.plugins.BW = {
 	init: function(config) {
 		var bacookie, cookies;
 
-		BOOMR.utils.pluginConfig(impl, config, "BW", ["base_url", "timeout", "nruns", "cookie", "cookie_exp"]);
+		BOOMR.utils.pluginConfig(impl, config, "BW",
+						["base_url", "timeout", "nruns", "cookie", "cookie_exp"]);
 
 		if(config && config.user_ip) {
 			impl.user_ip = config.user_ip;
@@ -1000,10 +1073,10 @@ BOOMR.plugins.BW = {
 		}
 
 		if(w.location.protocol === 'https:') {
-			// we don't run the test for https because SSL stuff will mess up b/w calculations
-			// we could run the test itself over HTTP, but then IE will complain about
-			// insecure resources, so the best is to just bail and hope that the user
-			// gets the cookie from some other page
+			// we don't run the test for https because SSL stuff will mess up b/w
+			// calculations // we could run the test itself over HTTP, but then IE
+			// will complain about insecure resources, so the best is to just bail
+			// and hope that the user/ gets the cookie from some other page
 
 			impl.complete = true;
 			return this;
@@ -1020,8 +1093,9 @@ BOOMR.plugins.BW = {
 
 	abort: function() {
 		impl.aborted = true;
-		impl.finish();	// we don't defer this call because it might be called from onbeforeunload
-				// and we want the entire chain to complete before we return
+		impl.finish();	// we don't defer this call because it might be called from
+				// onbeforeunload and we want the entire chain to complete
+				// before we return
 
 		return this;
 	},
