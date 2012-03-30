07d280a4c7626c420837ffbd52ae94f2f0169739
Philip Tellis <philip.tellis@gmail.com>
2012-03-30 09:41:21 -0400

    license file has a .txt extension

--- ipv6.js
+++ ipv6.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2011, Yahoo! Inc.  All rights reserved.
- * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ * Copyrights licensed under the BSD License. See the accompanying LICENSE.txt file for terms.
  */
 
 /**
