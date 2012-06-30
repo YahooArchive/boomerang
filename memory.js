0e8224539bd83143862b43b23a89065b9248b52e
Philip Tellis <philip.tellis@gmail.com>
2012-06-29 21:11:02 -0700

    Switch to window.opera
    Thanks to @mathiasbynens for the suggestion.  window.opera is more
    spoof-proof, and also way faster than doing a regex match, except for
    Safari, where they are more or less the same speed.

--- memory.js
+++ memory.js
@@ -23,7 +23,7 @@ var impl = {
 		var p = w.performance,
 		    c = w.console,
 		    d = w.document,
-		    f = (navigator.userAgent.match(/Opera/) ? d.querySelectorAll : d.getElementsByTagName),
+		    f = (({}).toString.call(window.opera) == '[object Opera]' ? d.querySelectorAll : d.getElementsByTagName),
 		    m;
 
 		m = (p && p.memory ? p.memory : (c && c.memory ? c.memory : null));
