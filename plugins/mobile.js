260a6dfbdc899c1172461a56cbd343559fb0709f
Philip Tellis <philip@lognormal.com>
2014-01-15 13:45:22 -0800

    Add bandwidth and metered as well from connection

--- plugins/mobile.js
+++ plugins/mobile.js
@@ -3,4 +3,18 @@
 Plugin to capture navigator.connection.type on browsers that support it
 */
 
-BOOMR.addVar("mob.ct", (typeof navigator === 'object' && navigator.connection) ? navigator.connection.type : 0);
+(function() {
+var connection;
+
+	if(typeof navigator === 'object') {
+		connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || navigator.msConnection;
+	}
+
+	if(!connection) {
+		return;
+	}
+	BOOMR.addVar("mob.ct", connection.type);
+	BOOMR.addVar("mob.bw", connection.bandwidth);
+	BOOMR.addVar("mob.mt", connection.metered);
+
+}());
