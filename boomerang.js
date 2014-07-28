e55a4a0e4b0618975d459e44f19283990bae9923
Philip Tellis <philip@lognormal.com>
2014-07-28 11:08:02 -0400

    Wrap event handler calls and init() calls in a try/catch
    We need to make sure that a failure in one plugin does not block other
    plugins from executing.  This change wraps each plugin call in a try/catch
    so that we catch any errors and log them, and then proceed with other plugins.

--- boomerang.js
+++ boomerang.js
@@ -246,8 +246,13 @@ impl = {
 		ev = this.events[e_name];
 
 		for(i=0; i<ev.length; i++) {
-			handler = ev[i];
-			handler.fn.call(handler.scope, data, handler.cb_data);
+			try {
+				handler = ev[i];
+				handler.fn.call(handler.scope, data, handler.cb_data);
+			}
+			catch(err) {
+				BOOMR.addError(err, 'fireEvent');
+			}
 		}
 
 		if (this.public_events.hasOwnProperty(e_name)) {
@@ -571,7 +576,12 @@ boomr = {
 
 				// plugin exists and has an init method
 				if(typeof this.plugins[k].init === "function") {
-					this.plugins[k].init(config);
+					try {
+						this.plugins[k].init(config);
+					}
+					catch(err) {
+						BOOMR.addError(err, this.plugins[k] + '.init');
+					}
 				}
 			}
 		}
