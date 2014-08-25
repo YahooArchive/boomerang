a7c32940814df2c0fb946ef27bb35e5ab8204193
Philip Tellis <philip@lognormal.com>
2014-08-25 17:13:44 -0400

    extra blank line at end of file

--- plugins/zzz_last_plugin.js
+++ plugins/zzz_last_plugin.js
@@ -2,4 +2,3 @@ BOOMR.t_end = new Date().getTime();
 
 /*jslint continue: true, plusplus: true, regexp: true, unparam: true, sloppy: true, white: true, browser: true, devel: true */
 /*properties BOOMR, BOOMR_lstart, BOOMR_start, BOOMR_configt*/
-
