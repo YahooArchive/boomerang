a55e1c4db7e24251aa03af9eb371d60f87b3fcc2
Andreas Marschke <andreas.marschke@gmail.com>
2014-10-11 21:12:24 +0200

    fix npm test command, fix Gruntfile.js according to default eslint values

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,37 @@
-'use strict';
+/* eslint-env node */
+
+"use strict";
 module.exports = function (grunt) {
 
-    var src = [ 'boomerang.js' ];
-    var plugins = grunt.file.readJSON('plugins.json');
+    var src = [ "boomerang.js" ];
+    var plugins = grunt.file.readJSON("plugins.json");
     src.push(plugins.plugins);
     src.push("plugins/zzz_last_plugin.js");
 
-    grunt.initConfig({ 
-	pkg:  grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+	pkg:  grunt.file.readJSON("package.json"),
 	concat: {
-	    options: { 
-		stripBanners: { 
+	    options: {
+		stripBanners: {
 		    block : true,
 		    line: true
 		},
-		seperator: ';'
+		seperator: ";"
 	    },
 	    debug: {
 		src: src,
-		dest: 'build/<%= pkg.name %>-<%= pkg.version %>-debug.js'
+		dest: "build/<%= pkg.name %>-<%= pkg.version %>-debug.js"
 	    },
 	    release: {
 		src: src,
-		dest: 'build/<%= pkg.name %>-<%= pkg.version %>.js'
+		dest: "build/<%= pkg.name %>-<%= pkg.version %>.js"
 	    }
 	},
 	eslint: {
 	    target: [
-		'Gruntfile.js',
-		'boomerang.js',
-		'plugins/*.js'
+		"Gruntfile.js",
+		"boomerang.js",
+		"plugins/*.js"
 	    ]
 	},
 	"string-replace": {
@@ -41,13 +43,13 @@ module.exports = function (grunt) {
 		options: {
 		    replacements: [{
 			pattern: /else{}/g,
-			replacement: ''
+			replacement: ""
 		    },{
 			pattern: /\(window\)\);/g,
-			replacement: '\(window\)\);\n'
+			replacement: "\(window\)\);\n"
 		    },{
 			pattern: /\(\)\);\(function\(/g,
-			replacement: '\(\)\);\n(function('
+			replacement: "\(\)\);\n(function("
 		    }]
 		}
 	    }
@@ -56,37 +58,31 @@ module.exports = function (grunt) {
 	    options : {
 		preserveComments: false,
 		mangle: false,
-		sourceMap: true,
+		sourceMap: true
 	    },
 	    min_release: {
-		report: 'gzip',
-		src: 'build/<%= pkg.name %>-<%= pkg.version %>.js',
-		dest: 'build/<%= pkg.name %>-<%= pkg.version %>.min.js'
+		report: "gzip",
+		src: "build/<%= pkg.name %>-<%= pkg.version %>.js",
+		dest: "build/<%= pkg.name %>-<%= pkg.version %>.min.js"
 	    },
 	    min_debug: {
-		report: 'gzip',
-		src: 'build/<%= pkg.name %>-<%= pkg.version %>-debug.js',
-		dest: 'build/<%= pkg.name %>-<%= pkg.version %>-debug.min.js'
-	    },
+		report: "gzip",
+		src: "build/<%= pkg.name %>-<%= pkg.version %>-debug.js",
+		dest: "build/<%= pkg.name %>-<%= pkg.version %>-debug.min.js"
+	    }
 	},
 	clean: {
 	    options: {},
-	    build: ['build/'],
-	    src: ['plugins/*~','*.js~']
+	    build: ["build/"],
+	    src: ["plugins/*~","*.js~"]
 	}
     });
 
-    grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-string-replace');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-string-replace");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
-    grunt.registerTask('default',['clean','concat','string-replace','uglify']);
+    grunt.registerTask("default",["clean","concat","string-replace","uglify"]);
 };
-
-
-
-
-
-
