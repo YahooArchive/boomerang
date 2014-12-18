d6dae1d953e0cfd23e308529275af1df7c9728d8
Nic Jansma <nic@nicj.net>
2014-12-18 12:10:24 -0500

    Misc small fixes for AutoXHR, added .editorconfig

--- plugins/auto_xhr.js
+++ plugins/auto_xhr.js
@@ -258,6 +258,10 @@ MutationHandler.prototype.wait_for_node = function(node, index) {
 				// img already loaded
 				return false;
 			}
+
+			if(node.getAttribute("src") === "" || node.getAttribute("src") === null) {
+				return false;
+			}
 		}
 
 		// no URL or javascript: or about: URL, so no network activity
