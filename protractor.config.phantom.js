226ed8140b949563ebadf54fc832b4cbc3bda103
Nic Jansma <nic@nicj.net>
2015-01-23 13:51:23 -0500

    TAP/Junit reporting for E2E tests

--- protractor.config.phantom.js
+++ protractor.config.phantom.js
@@ -5,5 +5,20 @@ exports.config = {
     capabilities: {
         "browserName": "phantomjs",
         "phantomjs.binary.path": require("phantomjs").path
-    }
+    },
+    onPrepare: function() {
+        // The require statement must be down here, since jasmine-reporters
+        // needs jasmine to be in the global and protractor does not guarantee
+        // this until inside the onPrepare function.
+        require('jasmine-reporters');
+
+        var tapReporter = new jasmine.TapReporter({
+            savePath: "tests/results/e2e.tap"
+        });
+
+        var junitReporter = new jasmine.JUnitXmlReporter("tests/results", false);
+
+        jasmine.getEnv().addReporter(tapReporter);
+        jasmine.getEnv().addReporter(junitReporter);
+    },
 };
