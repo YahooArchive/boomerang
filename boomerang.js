616c33f1f3494150acd6300f88a256e8ded93f7d
Charles Vazac <cvazac@soasta.com>
2015-01-23 08:34:56 -0800

    revert copyright change

--- boomerang.js
+++ boomerang.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2011, Yahoo! Inc.  All rights reserved.
  * Copyright (c) 2012, Log-Normal, Inc.  All rights reserved.
- * Copyright (c) 2014-2015, SOASTA, Inc. All rights reserved.
+ * Copyright (c) 2014, SOASTA, Inc. All rights reserved.
  * Copyrights licensed under the BSD License. See the accompanying LICENSE.txt file for terms.
  */
 
