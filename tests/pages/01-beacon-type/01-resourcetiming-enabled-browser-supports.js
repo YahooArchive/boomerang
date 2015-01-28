0c9153d0c0513d17556c861c61280f7bdeac59ce
Nic Jansma <nic@nicj.net>
2015-01-28 10:38:22 -0500

    File renaming

--- tests/pages/01-beacon-type/01-resourcetiming-enabled-browser-supports.js
+++ tests/pages/01-beacon-type/01-resourcetiming-enabled-browser-supports.js
@@ -1,7 +1,7 @@
 /*eslint-env mocha*/
 /*global BOOMR_test*/
 
-describe("e2e/01-beacon-type", function() {
+describe("e2e/01-beacon-type/01-resourcetiming-enabled-browser-supports", function() {
     it("Should send a FORM beacon if ResourceTiming is enabled and the browser supports it", function(done) {
         if (BOOMR_test.isResourceTimingSupported()) {
             BOOMR_test.validateBeaconWasForm(done);
--- tests/pages/01-beacon-type/02-resourcetiming-enabled-browser-unsupported.js
+++ tests/pages/01-beacon-type/02-resourcetiming-enabled-browser-unsupported.js
@@ -1,7 +1,7 @@
 /*eslint-env mocha*/
 /*global BOOMR_test*/
 
-describe("e2e/01-beacon-type", function() {
+describe("e2e/01-beacon-type/02-resourcetiming-enabled-browser-unsupported", function() {
     it("Should send an Image beacon if ResourceTiming is enabled, but the browser doesn't support it", function(done) {
         if (!BOOMR_test.isResourceTimingSupported()) {
             BOOMR_test.validateBeaconWasImg(done);
