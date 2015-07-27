b496132d27db75d7fdad365766e820d1c0d6d197
Andreas Marschke <andreas.marschke@gmail.com>
2015-07-27 22:18:06 +0200

    Make 07-autoxhr/02-onclick work on phantomjs

--- tests/page-templates/07-autoxhr/02-onclick.js
+++ tests/page-templates/07-autoxhr/02-onclick.js
@@ -4,32 +4,48 @@
 describe("e2e/07-autoxhr/02-onclick", function() {
 	var t = BOOMR_test;
 	var tf = BOOMR.plugins.TestFramework;
-
 	it("Should have sent at least 2 beacons, 1x onload, 1x xhr", function(done) {
-		this.timeout(10000);
-		t.ifAutoXHR(
-			done,
-			function() {
-				t.ensureBeaconCount(done, 2);
-				done();
-			});
+		if (window.MutationObserver && typeof window.MutationObserver === "function") {
+
+			t.ifAutoXHR(
+				done,
+				function() {
+					assert.lengthOf(tf.beacons, 2);
+					done();
+				}
+			);
+		}
+		else {
+			assert.lengthOf(tf.beacons, 1);
+			done();
+		}
 	});
 
 	it("Should get 2 beacons: 1st onload 2nd image fetch", function(done){
-		t.ifAutoXHR(
-			done,
-			function() {
-				assert.include(tf.beacons[0].u, "02-onclick.html");
-				done();
-			});
+		if (window.MutationObserver && typeof window.MutationObserver === "function") {
+			t.ifAutoXHR(
+				done,
+				function() {
+					assert.include(tf.beacons[0].u, "02-onclick.html");
+					done();
+				});
+		}
+		else {
+			done();
+		}
 	});
 
 	it("Should have a second beacon with the image URL in it", function(done){
-		t.ifAutoXHR(
-			done,
-			function() {
-				assert.include(tf.beacons[1].u, "img.jpg");
-				done();
-			});
+		if (window.MutationObserver && typeof window.MutationObserver === "function") {
+			t.ifAutoXHR(
+				done,
+				function() {
+					assert.include(tf.beacons[1].u, "img.jpg");
+					done();
+				});
+		}
+		else {
+			done();
+		}
 	});
 });
