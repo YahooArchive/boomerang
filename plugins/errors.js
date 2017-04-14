5f2a31a2c7dcee4178770052e9325838b45a12a5
Nigel Heron <nheron@querymetrics.com>
2017-04-14 06:13:05 -0700

    Deduplicate calls to AddEventListener with the same arguments

--- plugins/errors.js
+++ plugins/errors.js
@@ -837,10 +837,16 @@
 					args[callbackIndex] = wrappedFn;
 
 					if (functionName === "addEventListener") {
-						// for removeEventListener we need to keep track of this
+						// For removeEventListener we need to keep track of this
 						// unique tuple of target object, event name (arg0), original function
 						// and capture (arg2)
-						impl.trackFn(targetObj, args[0], callbackFn, args[2], wrappedFn);
+						// Since we wrap the origFn with a new anonymous function we can't rely on
+						// the browser's addEventListener to dedup multiple additions of the same
+						// callback.
+						if (!impl.trackFn(targetObj, args[0], callbackFn, args[2], wrappedFn)) {
+							// if the callback is already tracked, we won't call addEventListener
+							return;
+						}
 					}
 
 					return origFn.apply(targetObj, args);
@@ -860,15 +866,17 @@
 		 * @param {function} listener Original listener
 		 * @param {boolean|object} useCapture|options Use capture flag or options object
 		 * @param {function} wrapped Wrapped function
+		 *
+		 * @returns {boolean} - `true` if function is not already tracked, false otherwise
 		 */
 		trackFn: function(target, type, listener, useCapture, wrapped) {
 			if (!target) {
-				return;
+				return false;
 			}
 
 			if (impl.trackedFnIdx(target, type, listener, useCapture) !== -1) {
 				// already tracked
-				return;
+				return false;
 			}
 
 			if (!target._bmrEvents) {
