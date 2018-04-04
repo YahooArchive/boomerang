de2de76797d9547b91b762be8e4fa1d0a42d7ae9
Philip Tellis <philip.tellis@gmail.com>
2018-04-04 10:33:13 -0400

    Handle latest netinfo spec

--- plugins/mobile.js
+++ plugins/mobile.js
@@ -17,100 +17,51 @@
  * @class BOOMR.plugins.Mobile
  */
 (function() {
+	var connection, param_map = {
+		"type": "ct",
+		"bandwidth": "bw",
+		"metered": "mt",
+		"effectiveType": "etype",
+		"downlinkMax": "lm",
+		"downlink": "dl",
+		"rtt": "rtt"
+	};
+
 	BOOMR = window.BOOMR || {};
-	BOOMR.plugins = BOOMR.plugins || {};
 
-	var impl = {
-		connection: null,
-		param_map: {
-			"type": "ct",
-			"bandwidth": "bw",
-			"metered": "mt",
-			"effectiveType": "etype",
-			"downlinkMax": "lm",
-			"downlink": "dl",
-			"rtt": "rtt",
-			"saveData": "sd"
-		},
+	if (typeof BOOMR.addVar !== "function") {
+		return;
+	}
 
-		/**
-		 * Get the connection information object if available.
-		 *
-		 * @returns {null|NavigatorNetworkInformation} The network information API data.
-		 */
-		getConnection: function() {
-			if (this.connection) {
-				return this.connection;
-			}
+	if (typeof navigator === "object") {
+		connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || navigator.msConnection;
+	}
 
-			if (typeof navigator === "object") {
-				this.connection = navigator.connection ||
-					navigator.mozConnection ||
-					navigator.webkitConnection ||
-					navigator.msConnection;
-			}
+	if (!connection) {
+		return;
+	}
 
-			return this.connection;
-		},
+	function setVars() {
+		var k;
 
-		/**
-		 * Add listener to update network information when it changes.
-		 *
-		 * @returns {void}
-		 */
-		subscribe: function() {
-			var connection = this.getConnection();
-
-			if (connection.addEventListener) {
-				connection.addEventListener("change", function() {
-					impl.setConnectionInformation();
-					BOOMR.fireEvent("netinfo", connection);
-				});
-			}
-		},
-
-		/**
-		 * Set connection information.
-		 *
-		 * Uses information from the Network Information API to add variable to the beacon. This data is set once the plugin
-		 * is initiated and is only updated if the network information changes.
-		 *
-		 * @returns {void}
-		 */
-		setConnectionInformation: function() {
-			var k, connection, type, param_map = this.param_map;
-
-			connection = this.getConnection();
-
-			for (k in param_map) {
-				if (k in connection) {
-					// Remove old parameter value from the beacon because new value might be falsey which won't overwrite old value
-					BOOMR.removeVar("mob." + param_map[k]);
-
-					type = typeof connection[k];
-					if (type === "number" || type === "string" || type === "boolean") {
-						BOOMR.addVar("mob." + param_map[k], connection[k]);
-					}
+		for (k in param_map) {
+			if (connection.hasOwnProperty(k)) {
+				// Remove old parameter value from the beacon because new value might be falsy which won't overwrite old value
+				BOOMR.removeVar("mob." + param_map[k]);
+				if (connection[k]) {
+					BOOMR.addVar("mob." + param_map[k], connection[k]);
 				}
 			}
 		}
-	};
+	}
 
-	BOOMR.plugins.Mobile = {
-		init: function() {
-			var connection = impl.getConnection();
+	// If connection information changes, we collect the latest values
+	if (connection.addEventListener) {
+		connection.addEventListener("change", function() {
+			setVars();
+			BOOMR.fireEvent("netinfo", connection);
+		});
+	}
 
-			// Only get information if we have a source for the information
-			if (connection) {
-				impl.setConnectionInformation();
-				impl.subscribe();
-			}
-
-			return this;
-		},
-
-		is_complete: function() {
-			return true;
-		}
-	};
+	setVars();
 }());
