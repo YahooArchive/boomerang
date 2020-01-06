43a2e87f3612c83cb632278d87f441190ae2e55a
Nic Jansma <nic@nicj.net>
2020-01-06 14:38:09 -0500

    Removed additional SPA tests not needed in OS repo

--- tests/page-templates/05-angular/22-config-after-onload.js
+++
@@ -1,6 +0,0 @@
-/*eslint-env mocha*/
-/*global BOOMR_test*/
-
-describe("e2e/05-angular/22-config-after-onload", function() {
-	BOOMR_test.templates.SPA["22-config-after-onload"]();
-});
--- tests/page-templates/09-backbone/22-config-after-onload.js
+++
@@ -1,6 +0,0 @@
-/*eslint-env mocha*/
-/*global BOOMR_test*/
-
-describe("e2e/09-backbone/22-config-after-onload", function() {
-	BOOMR_test.templates.SPA["22-config-after-onload"]();
-});
