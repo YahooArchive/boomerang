329679a341c31714c42550882d5d1b9a3ae40a6a
Nic Jansma <nic@nicj.net>
2022-03-30 11:10:11 -0400

    Tests: Fixes 12-react/108-hard-nav-disable

--- plugins/spa.js
+++ plugins/spa.js
@@ -418,7 +418,7 @@
 			// If this was the first request, use navStart as the begin timestamp.  Otherwise, use
 			// "now" as the begin timestamp.
 			var navigationStart = (BOOMR.plugins.RT && BOOMR.plugins.RT.navigationStart());
-			var requestStart = firstSpaNav ? navigationStart : now;
+			var requestStart = (firstSpaNav && !initialRouteChangeCompleted) ? navigationStart : now;
 
 			// use the document.URL even though it may be the URL of the previous nav. We will updated
 			// it in AutoXHR sendEvent
