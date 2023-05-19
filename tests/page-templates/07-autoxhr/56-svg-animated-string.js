724474abd9d10359eaf4a30d5c67fb215c738217
Nic Jansma <nic@nicj.net>
2023-05-19 12:50:29 -0400

    Lint: Lint fixes

--- tests/page-templates/07-autoxhr/56-svg-animated-string.js
+++ tests/page-templates/07-autoxhr/56-svg-animated-string.js
@@ -2,23 +2,23 @@
 /* global assert */
 
 describe("e2e/07-autoxhr/56-svg-animated-string", function() {
-  var t = BOOMR_test;
-  var tf = BOOMR.plugins.TestFramework;
+	var t = BOOMR_test;
+	var tf = BOOMR.plugins.TestFramework;
 
-  it("Should get 1 beacon", function(done) {
-    this.timeout(10000);
+	it("Should get 1 beacon", function(done) {
+		this.timeout(10000);
 
-    t.ensureBeaconCount(done, 1);
-  });
+		t.ensureBeaconCount(done, 1);
+	});
 
-  it("Should not have fired an error", function(done) {
-    t.ifAutoXHR(
-      done,
-      function() {
-        assert.isUndefined(window.onerrorFired);
+	it("Should not have fired an error", function(done) {
+		t.ifAutoXHR(
+		done,
+		function() {
+			assert.isUndefined(window.onerrorFired);
 
-        done();
-      },
-      this.skip.bind(this));
-  });
+			done();
+		},
+		this.skip.bind(this));
+	});
 });
